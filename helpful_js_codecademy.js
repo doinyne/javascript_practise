ef5136a966133bf1864517a483f1b54350741655
dean <d.lewis.rave@gmail.com>
2021-11-23 12:39:38 +0000

    Added some truthly and falsly

--- helpful_js_codecademy.js
+++ helpful_js_codecademy.js
@@ -44,3 +44,78 @@ console.log(!excited); // Prints false
  
 let sleepy = false;
 console.log(!sleepy); // Prints true
+
+// example of && logical operator
+
+let mood = 'sleepy';
+let tirednessLevel = 6;
+
+if (mood === 'sleepy' && tirednessLevel > 8) {
+  console.log('time to sleep'); 
+  } else {
+    console.log('not bed time yet');
+  }
+
+// truthly and falsly values 
+
+let myVariable = 'I Exist!';
+ 
+if (myVariable) {
+   console.log(myVariable)
+} else {
+   console.log('The variable does not exist.')
+}
+
+/*
+The list of falsy values includes:
+
+0
+Empty strings like "" or ''
+null which represent when there is no value at all
+undefined which represent when a declared variable lacks a value
+NaN, or Not a Number
+*/
+
+let numberOfApples = 0;
+ 
+if (numberOfApples){
+   console.log('Let us eat apples!');
+} else {
+   console.log('No apples left!');
+}
+ 
+// Prints 'No apples left!'
+
+// a truthly example
+
+let wordCount = 1000;
+
+if (wordCount) {
+  console.log("Great! You've started your work!");
+} else {
+  console.log('Better get to work!');
+}
+
+
+let favoritePhrase = 'Hello World!';
+
+if (favoritePhrase) {
+  console.log("This string doesn't seem to be empty.");
+} else {
+  console.log('This string is definitely empty.');
+}
+
+Great! You've started your work!
+This string doesn't seem to be empty.
+
+// a falsely example
+
+let favoritePhrase = '';
+
+if (favoritePhrase) {
+  console.log("This string doesn't seem to be empty.");
+} else {
+  console.log('This string is definitely empty.');
+}
+
+This string is definitely empty.
