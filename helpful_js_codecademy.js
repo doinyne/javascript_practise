c83a3fe44102283f719cf8dd5bcc430bf7312177
dean <d.lewis.rave@gmail.com>
2021-11-23 11:41:15 +0000

    codecademy code

---
+++ helpful_js_codecademy.js
@@ -0,0 +1,22 @@
+// helpful JS from codecademy 
+
+// simple if else statment 
+
+let hungerLevel = 7;
+
+if (hungerLevel > 7) {
+  console.log('Time to eat!');
+} else {
+  console.log('We can eat later!');
+}
+
+// return positive or negative numbers
+
+function myNumberSign(a) {
+  if a > 0 
+    return 'positive'
+  else if a < 0 
+    return 'negative'
+  else
+    return 0
+}
