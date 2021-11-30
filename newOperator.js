53e451e995f40e816f3d190a2eb7efe6d576255d
dean <d.lewis.rave@gmail.com>
2021-11-30 12:36:21 +0000

    New function

--- newOperator.js
+++ newOperator.js
@@ -1,14 +1,34 @@
-function Car(make, model, year) {
-  this.make = make;
-  this.model = model;
-  this.year = year;
-}
+// function Car(make, model, year) {
+//   this.make = make;
+//   this.model = model;
+//   this.year = year;
+// }
 
-const car1 = new Car('Eagle', 'Talon TSi', 1993);
+// const car1 = new Car('Eagle', 'Talon TSi', 1993);
 
-console.log(car1.year);
-console.log(car1.model);
-console.log(car1.make);
+// console.log(car1.year);
+// console.log(car1.model);
+// console.log(car1.make);
 // expected output: "Eagle"
 
 // 'the syntax' new constructor[([arguments])]
+
+// constructor
+// A class or function that specifies the type of the object instance.
+
+// arguments
+// A list of values that the constructor will be called with.
+
+function Person(name, age, car, location) {
+  this.name = name;
+  this.age = age;
+  this.car = car;
+  this.location = location;
+}
+
+let dean = new Person('Dean', 27, 'Ford', 'London');
+
+console.log(dean.name);
+console.log(dean.age);
+console.log(dean.car);
+console.log(dean.location);
